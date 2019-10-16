d2a5f1d55adab6a2c80c3eea00a76b45e7b67d05
emmdim <emmdim@users.noreply.github.com>
2019-10-17 00:03:28 +0200

    Newsfeed WIP

--- components/page-newsfeed-new.tsx
+++ components/page-newsfeed-new.tsx
@@ -1,5 +1,5 @@
 import { Component } from "react"
-import { Button, Input, Form, Select, InputNumber, message } from 'antd'
+import { Button, Input, Form, Table, Select, InputNumber, message } from 'antd'
 import { headerBackgroundColor } from "../lib/constants"
 import { JsonFeedPost, API, Network } from "dvote-js"
 import EthereumManager from "../util/ethereum-manager"
@@ -33,9 +33,9 @@ const formItemLayout = {
 }
 
 export default class PageNewsFeedNew extends Component<Props, State> {
-    // state = {
-    //     selectedPost: this.makeEmptyProcess()
-    // }
+    state = {
+         selectedPost: this.makeEmptyPost()
+    }
 
     submit = () => {
         // let success = this.checkFields()
@@ -68,15 +68,40 @@ export default class PageNewsFeedNew extends Component<Props, State> {
         return this.setNestedKey(obj[path[0]], path.slice(1), value)
     }
 
-    // setselectedPostField(path, value) {
-    //     let process = this.cloneselectedPost()
-    //     this.setNestedKey(process, path, value)
-    //     this.setState({ selectedPost: process })
-    // }
+    cloneselectedPost() {
+        return Object.assign({}, this.state.selectedPost)
+    }
+
+    setselectedPostField(path, value) {
+        let post = this.cloneselectedPost()
+        this.setNestedKey(process, path, value)
+        this.setState({ selectedPost: post })
+    }
+
+    makeEmptyPost() {
+        let post: JsonFeedPost = {
+            id: "",
+            title: "",
+            summary: "",
+            content_text: "",
+            content_html: "",
+            url: "",
+            image: "",
+            tags: [],
+            date_published: "",
+            date_modified: "",
+            author: {
+                name: "",
+                url: "",
+            }
+        }
+        return post
+    }
 
     renderPostEdit() {
         return null
-        // const currentPost = {}
+        
+        // const currentPost = this.state.selectedPost
 
         // return <div style={{ padding: 30 }}>
         //     <h2>General</h2>
@@ -85,79 +110,82 @@ export default class PageNewsFeedNew extends Component<Props, State> {
         //             <Input
         //                 style={fieldStyle}
         //                 size="large"
-        //                 placeholder="Human Rights Declaration"
-        //                 value={currentPost.details.title.default}
-        //             // onChange={ev => this.setselectedPostField(['details', 'title', 'default'], ev.target.value)}
+        //                 placeholder="Post Title"
+        //                 value={currentPost.title}
+        //                 onChange={ev => this.setselectedPostField(['title'], ev.target.value)}
         //             />
         //         </Form.Item>
-        //         <Form.Item label="Type">
-        //             <Select
-        //                 showSearch
-        //                 style={{ width: 200 }}
-        //                 value={currentPost.type}
-        //                 placeholder="Select a process type"
-        //             // onChange={value => this.setselectedPostField(['type'], value)}
-        //             >
-        //                 <Select.Option value="snark-vote" disabled>Anonymous Vote</Select.Option>
-        //                 <Select.Option value="poll-vote">Poll</Select.Option>
-        //                 <Select.Option value="petition-sign" disabled>Petition</Select.Option>
-        //             </Select>
-        //         </Form.Item>
-        //         <Form.Item label="Description">
+        //         <Form.Item label="Summary">
         //             <TextArea
         //                 style={fieldStyle}
-        //                 placeholder="Description"
-        //                 autosize={{ minRows: 4, maxRows: 8 }}
-        //                 value={currentPost.details.description['default']}
-        //             // onChange={ev => this.setselectedPostField(["details", "description", "default"], ev.target.value)}
+        //                 placeholder="Summary"
+        //                 autosize={{ minRows: 2, maxRows: 4 }}
+        //                 value={currentPost.summary}
+        //                 onChange={ev => this.setselectedPostField(["summary"], ev.target.value)}
         //             />
         //         </Form.Item>
-        //         <Form.Item label="Census Merkle Root">
+        //         <Form.Item label="Text">
+        //             <TextArea
+        //                 style={fieldStyle}
+        //                 placeholder="Text"
+        //                 autosize={{ minRows: 2, maxRows: 8 }}
+        //                 value={currentPost.content_text}
+        //                 onChange={ev => this.setselectedPostField(["content_text"], ev.target.value)}
+        //             />
+        //         </Form.Item>
+        //         <Form.Item label="HTML">
+        //             <TextArea
+        //                 style={fieldStyle}
+        //                 placeholder="HTML"
+        //                 autosize={{ minRows: 2, maxRows: 8 }}
+        //                 value={currentPost.content_html}
+        //                 onChange={ev => this.setselectedPostField(["content_html"], ev.target.value)}
+        //             />
+        //         </Form.Item>
+        //         <Form.Item label="URL">
         //             <Input
         //                 style={fieldStyle}
-        //                 size="large"
-        //                 placeholder="0x123456789..."
-        //                 value={currentPost.census.merkleRoot}
-        //             // onChange={ev => this.setselectedPostField(['census', 'merkleRoot'], ev.target.value)}
+        //                 placeholder="http://link.item/1234"
+        //                 value={currentPost.url}
+        //                 onChange={ev => this.setselectedPostField(["url"], ev.target.value)}
         //             />
-        //             <p style={{ marginBottom: 0 }}><small>You should find this value on Vocdoni's Census Manager or in your organization CRM</small></p>
         //         </Form.Item>
-        //         <Form.Item label="Census Merkle Tree origin">
+        //         <Form.Item label="Image">
         //             <Input
         //                 style={fieldStyle}
-        //                 size="large"
-        //                 placeholder="ipfs://123456...!12345678"
-        //                 value={currentPost.census.merkleTree}
-        //             // onChange={ev => this.setselectedPostField(['census', 'merkleTree'], ev.target.value)}
-        //             />
-        //             <p style={{ marginBottom: 0 }}><small>You should find this value on Vocdoni's Census Manager or in your organization CRM</small></p>
-        //         </Form.Item>
-        //         <Form.Item label="Start block">
-        //             <InputNumber
-        //                 style={fieldStyle}
-        //                 min={0}
-        //                 placeholder="1234"
-        //                 value={currentPost.startBlock}
-        //             // onChange={num => this.setselectedPostField(["startBlock"], num)}
-        //             />
-
-        //         </Form.Item>
-        //         <Form.Item label="Number of blocks">
-        //             <InputNumber
-        //                 style={fieldStyle}
-        //                 min={1}
-        //                 placeholder="200"
-        //                 value={currentPost.numberOfBlocks}
-        //             // onChange={num => this.setselectedPostField(["numberOfBlocks"], num)}
+        //                 placeholder="http://link.item/1234"
+        //                 value={currentPost.image}
+        //                 onChange={ev => this.setselectedPostField(["image"], ev.target.value)}
         //             />
         //         </Form.Item>
-
-        //         <Form.Item label="Header image URI">
+                
+        //         {/* <Form.Item label="Tags">
+        //             <ArrayInput source="my-source">
+        //                 <SimpleFormIterator>
+        //                 <Input
+        //                     style={fieldStyle}
+        //                     placeholder="Name"
+        //                     value={currentPost.author.name}
+        //                     onChange={ev => this.setselectedPostField(["tags"], ev.target.value)}
+        //                 />
+        //                 </SimpleFormIterator>
+        //             </ArrayInput>
+                    
+        //         </Form.Item> */}
+        //         <Form.Item label="Author Name">
         //             <Input
         //                 style={fieldStyle}
-        //                 placeholder="Header image Uri"
-        //                 value={currentPost.details.headerImage}
-        //             // onChange={ev => this.setselectedPostField(["details", "headerImage"], ev.target.value)}
+        //                 placeholder="Name"
+        //                 value={currentPost.author.name}
+        //                 onChange={ev => this.setselectedPostField(["author", "name"], ev.target.value)}
+        //             />
+        //         </Form.Item>
+        //         <Form.Item label="Author URL">
+        //             <Input
+        //                 style={fieldStyle}
+        //                 placeholder="URL"
+        //                 value={currentPost.author.url}
+        //                 onChange={ev => this.setselectedPostField(["author", "url"], ev.target.value)}
         //             />
         //         </Form.Item>
         //     </Form>
