4fd5d289763516b1b18304055ca782060e482d7f
dappfuse <5128827-dappfuse@users.noreply.gitlab.com>
2020-03-25 18:25:47 +0100

    Entity show

--- pages/entities/index.tsx
+++ pages/entities/index.tsx
@@ -1,6 +1,11 @@
 import { useContext, Component } from 'react'
 import AppContext, { IAppContext } from '../../components/app-context'
-// import Link from "next/link"
+import { message, Spin } from 'antd'
+import { getGatewayClients } from '../../lib/gateways'
+import { API, EntityMetadata } from "dvote-js"
+import { Divider } from '../../components/divider'
+const { Entity } = API
+import Link from "next/link"
 // import MainLayout from "../../components/layout"
 // import { main } from "../i18n"
 // import MultiLine from '../components/multi-line-text'
@@ -15,26 +20,62 @@ const EntityViewPage = props => {
 }
 
 type State = {
-  id?: string
-  // TODO:
+  entityLoading?: boolean,
+  entity?: EntityMetadata,
+  entityId?: string,
 }
 
 // Stateful component
 class EntityView extends Component<IAppContext, State> {
   state: State = {}
 
-  componentDidMount() {
-    this.setState({ id: location.hash.substr(2) })
+  async componentDidMount() {
+    this.props.setTitle("Loading")
 
-    // TODO: FETCH METADATA
+    try {
+      const entityId = location.hash.substr(2)
+      this.setState({ entityLoading: true, entityId })
 
-    this.props.setTitle(`Entity ${location.hash.substr(2)}`)
+      const { web3Gateway, dvoteGateway } = await getGatewayClients()
+      const entity = await Entity.getEntityMetadata(entityId, web3Gateway, dvoteGateway)
+      if (!entity) throw new Error()
+
+      this.setState({ entity, entityId, entityLoading: false })
+      this.props.setTitle(entity.name["default"])
+    }
+    catch (err) {
+      this.setState({ entityLoading: false })
+      message.error("Could not read the entity metadata")
+    }
+  }
+
+  renderEntityInfo() {
+    return <>
+      <img src={this.state.entity.media.avatar} className="avatar" />
+      <h4>{this.state.entity.name["default"]}</h4>
+      <p>{this.state.entity.description["default"]}</p>
+      <pre>{JSON.stringify(this.state.entity, null, 2)}</pre>
+      {/* <p><Link href={`/entities/edit/#/${this.state.entityId}`}><a><Button>Manage my entity</Button></a></Link></p> */}
+    </>
+  }
+
+  renderNotFound() {
+    return <>
+      <h4>Entity not found</h4>
+      <p>The entity you are looking for cannot be found</p>
+    </>
+  }
+
+  renderLoading() {
+    return <div>Please, wait... <Spin size="small" /></div>
   }
 
   render() {
     return <div id="entity-view">
-      <p>Entity view</p>
-      <pre>I am {this.state.id}</pre>
+      {
+        this.state.entityLoading ? this.renderLoading() :
+          this.state.entity ? this.renderEntityInfo() : this.renderNotFound()
+      }
     </div>
   }
 }
