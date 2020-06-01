5dd3d95e14f0bca431233a269f017851376db06c
dappfuse <5128827-dappfuse@users.noreply.gitlab.com>
2020-06-01 17:56:06 +0200

    Minor tweaks

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import AppContext from './app-context'
 import { useContext } from "react"
 import { Layout, Button, Row, Col } from "antd"
 import SideMenu from "./side-menu"
-import UserMenu from "./user-menu"
+import AccountMenu from "./account-menu"
 import { MenuOutlined } from "@ant-design/icons"
 
 
@@ -22,7 +22,7 @@ export default function ({ children, ...props }: Props) {
     return <Layout id="layout">
         <Layout.Header className="top-bar">
             <Row>
-                { context.menuVisible && 
+                { context.menuVisible &&
                     <Col xs={2} md={0}>
                         <Button ghost={true} onClick={() => context.setMenuCollapsed(!context.menuCollapsed)}>
                             <MenuOutlined />
@@ -33,18 +33,18 @@ export default function ({ children, ...props }: Props) {
                     <h1 style={{marginLeft:20}}><Link href="/"><a id="logo">{title || "Entities"}</a></Link></h1>
                 </Col>
                 <Col xs={10} style={{textAlign: "right"}}>
-                    <UserMenu />
+                    <AccountMenu />
                 </Col>
             </Row>
         </Layout.Header>
         <Layout>
             {context.menuVisible && <Layout.Sider style={{height:"100vh"}}
-                width={200} 
-                collapsible 
-                breakpoint="md" 
-                collapsedWidth={0} 
-                trigger={null} 
-                collapsed={context.menuCollapsed} 
+                width={200}
+                collapsible
+                breakpoint="md"
+                collapsedWidth={0}
+                trigger={null}
+                collapsed={context.menuCollapsed}
                 onCollapse={() => context.setMenuCollapsed(!context.menuCollapsed)}
             >
                 <SideMenu />
