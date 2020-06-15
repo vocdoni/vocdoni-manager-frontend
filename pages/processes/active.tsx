f4bdef4f21615b840a375a94e569662ba5690fe0
dappfuse <5128827-dappfuse@users.noreply.gitlab.com>
2020-06-15 20:37:51 +0200

    Ensure that ending processes actually cancels them

--- pages/processes/active.tsx
+++ pages/processes/active.tsx
@@ -27,216 +27,215 @@ const PAGE_SIZE = 6
 
 // MAIN COMPONENT
 const ProcessActiveViewPage = props => {
-	// Get the global context and pass it to our stateful component
-	const context = useContext(AppContext)
+    // Get the global context and pass it to our stateful component
+    const context = useContext(AppContext)
 
-	return <ProcessActiveView {...context} />
+    return <ProcessActiveView {...context} />
 }
 
 type State = {
-	dataLoading?: boolean,
-	entity?: EntityMetadata,
-	entityId?: string,
-	processes: ({ id: string, data: ProcessMetadata } | string)[],
-	startIndex: number
+    dataLoading?: boolean,
+    entity?: EntityMetadata,
+    entityId?: string,
+    processes: ({ id: string, data: ProcessMetadata } | string)[],
+    startIndex: number
 }
 
 // Stateful component
 class ProcessActiveView extends Component<IAppContext, State> {
-	state: State = {
-		startIndex: 0,
-		processes: []
-	}
+    state: State = {
+        startIndex: 0,
+        processes: []
+    }
 
-	async componentDidMount() {
-		await this.fetchData()
-	}
+    async componentDidMount() {
+        await this.fetchData()
+    }
 
-	async fetchData(){
-		try {
-			this.props.setMenuSelected("processes-active")
+    async fetchData() {
+        try {
+            this.props.setMenuSelected("processes-active")
 
-			const entityId = location.hash.substr(2)
-			this.setState({ dataLoading: true, entityId })
+            const entityId = location.hash.substr(2)
+            this.setState({ dataLoading: true, entityId })
 
-			const gateway = await getGatewayClients()
-			const entity = await Entity.getEntityMetadata(entityId, gateway)
-			if (!entity) throw new Error()
+            const gateway = await getGatewayClients()
+            const entity = await Entity.getEntityMetadata(entityId, gateway)
+            if (!entity) throw new Error()
 
-			const processIds = entity.votingProcesses.active || []
-			this.setState({ processes: processIds })
+            const processIds = entity.votingProcesses.active || []
+            this.setState({ processes: processIds })
 
-			await Promise.all((processIds).map(id => {
-				return getVoteMetadata(id, gateway).then(voteMetadata => {
-					const updatedProcesses: ({ id: string, data: ProcessMetadata } | string)[] = [].concat(this.state.processes)
-					for (let i = 0; i < this.state.processes.length; i++) {
-						if (typeof updatedProcesses[i] == "string" && updatedProcesses[i] == id) {
-							updatedProcesses[i] = { id, data: voteMetadata }
-							this.setState({ processes: updatedProcesses })
-							break
-						}
-					}
-				}).catch(err => {
-					if (err && err.message == "Request timed out") throw err
-					throw new Error("failed")
-				})
-			}))
+            await Promise.all((processIds).map(id => {
+                return getVoteMetadata(id, gateway).then(voteMetadata => {
+                    const updatedProcesses: ({ id: string, data: ProcessMetadata } | string)[] = [].concat(this.state.processes)
+                    for (let i = 0; i < this.state.processes.length; i++) {
+                        if (typeof updatedProcesses[i] == "string" && updatedProcesses[i] == id) {
+                            updatedProcesses[i] = { id, data: voteMetadata }
+                            this.setState({ processes: updatedProcesses })
+                            break
+                        }
+                    }
+                }).catch(err => {
+                    if (err && err.message == "Request timed out") throw err
+                    throw new Error("failed")
+                })
+            }))
 
-			this.setState({ entity, entityId, dataLoading: false })
-			this.props.setTitle(entity.name["default"])
-			this.props.setEntityId(entityId)
-		}
-		catch (err) {
-			this.setState({ dataLoading: false })
+            this.setState({ entity, entityId, dataLoading: false })
+            this.props.setTitle(entity.name["default"])
+            this.props.setEntityId(entityId)
+        }
+        catch (err) {
+            this.setState({ dataLoading: false })
 
-			if (err && err.message == "Request timed out")
-				message.error("The list of voting processes took too long to load")
-			else if (err && err.message == "failed")
-				message.error("One of the processes could not be loaded")
-			else
-				message.error("The list of voting processes could not be loaded")
-		}
-	}
+            if (err && err.message == "Request timed out")
+                message.error("The list of voting processes took too long to load")
+            else if (err && err.message == "failed")
+                message.error("One of the processes could not be loaded")
+            else
+                message.error("The list of voting processes could not be loaded")
+        }
+    }
 
-	shouldComponentUpdate(){
+    shouldComponentUpdate() {
         const entityId = location.hash.substr(2)
         if (entityId != this.state.entityId) {
             this.fetchData()
         }
-        
+
         return true
     }
-  
-	confirmMarkAsEnded(processId: string) {
-		var that = this;
-		Modal.confirm({
-			title: "Confirm",
-			icon: <ExclamationCircleOutlined />,
-			content: "The process will be marked as ended and the vote scrutiny will be triggered (if necessary). Do you want to continue?",
-			okText: "Mark as ended",
-			okType: "primary",
-			cancelText: "Not now",
-			onOk() {
-				that.markAsEnded(processId)
-			},
-		})
-	}
 
-	async markAsEnded(processId: string) {
-		let activeProcesses = JSON.parse(JSON.stringify(this.state.entity.votingProcesses.active))
-		let endedProcesses = JSON.parse(JSON.stringify(this.state.entity.votingProcesses.ended))
-		activeProcesses = activeProcesses.filter(id => id != processId)
-		endedProcesses.unshift(processId)
+    confirmMarkAsEnded(processId: string) {
+        var that = this;
+        Modal.confirm({
+            title: "Confirm",
+            icon: <ExclamationCircleOutlined />,
+            content: "The process will be marked as ended and the vote scrutiny will be triggered (if necessary). Do you want to continue?",
+            okText: "Mark as ended",
+            okType: "primary",
+            cancelText: "Not now",
+            onOk() {
+                that.markAsEnded(processId)
+            },
+        })
+    }
 
-		const hideLoading = message.loading('Ending the process...', 0)
+    async markAsEnded(processId: string) {
+        const hideLoading = message.loading('Ending the process...', 0)
+        try {
+            const gateway = await getGatewayClients()
 
-		try {
-			const gateway = await getGatewayClients()
+            // Cancel if still needed
+            if (!(await isCanceled(processId, gateway))) {
+                await cancelProcess(processId, this.props.web3Wallet.getWallet() as (Wallet | Signer), gateway)
+            }
 
-			// TODO: Check if the process has actually ended before proceeding consulting the
-			// actual block number of Vochain and the finishBlock
+            // Relist
+            let activeProcesses = JSON.parse(JSON.stringify(this.state.entity.votingProcesses.active))
+            let endedProcesses = JSON.parse(JSON.stringify(this.state.entity.votingProcesses.ended))
+            activeProcesses = activeProcesses.filter(id => id != processId)
+            endedProcesses.unshift(processId)
 
-			let entityMetadata = this.state.entity
-			entityMetadata.votingProcesses.active = activeProcesses
-			entityMetadata.votingProcesses.ended = endedProcesses
+            let entityMetadata = this.state.entity
+            entityMetadata.votingProcesses.active = activeProcesses
+            entityMetadata.votingProcesses.ended = endedProcesses
 
-			const address = this.props.web3Wallet.getAddress()
-			await updateEntity(address, entityMetadata, this.props.web3Wallet.getWallet() as (Wallet | Signer), gateway)
+            const address = this.props.web3Wallet.getAddress()
+            await updateEntity(address, entityMetadata, this.props.web3Wallet.getWallet() as (Wallet | Signer), gateway)
 
-			if (!(await isCanceled(processId, gateway))) {
-				await cancelProcess(processId, this.props.web3Wallet.getWallet() as (Wallet | Signer), gateway)
-			}
-			hideLoading()
+            hideLoading()
 
-			message.success("The process has ended successfully")
-			this.componentDidMount() // reload process list
-		}
-		catch (err) {
-			hideLoading()
-			console.error("The process could not be ended", err)
-			message.error("The process could not be ended")
-		}
+            message.success("The process has ended successfully")
+            this.componentDidMount() // reload process list
+        }
+        catch (err) {
+            hideLoading()
+            console.error("The process could not be ended", err)
+            message.error("The process could not be ended")
+        }
 
-	}
+    }
 
-	renderProcessesList() {
-		const entityId = location.hash.substr(2)
-		const address = this.props.web3Wallet.getAddress()
-		const { readOnly } = getNetworkState()
-		let hideEditControls = readOnly || !address
-		if (!hideEditControls) {
-			const ownEntityId = getEntityId(address)
-			hideEditControls = this.state.entityId != ownEntityId
-		}
+    renderProcessesList() {
+        const entityId = location.hash.substr(2)
+        const address = this.props.web3Wallet.getAddress()
+        const { readOnly } = getNetworkState()
+        let hideEditControls = readOnly || !address
+        if (!hideEditControls) {
+            const ownEntityId = getEntityId(address)
+            hideEditControls = this.state.entityId != ownEntityId
+        }
 
-		return <div className="body-card">
-			<Divider orientation="left">Active votes</Divider>
+        return <div className="body-card">
+            <Divider orientation="left">Active votes</Divider>
 
-			<List
-				itemLayout="vertical"
-				size="large"
-				pagination={{
-					onChange: page => {
-						this.setState({ startIndex: (page - 1) * PAGE_SIZE })
-						window.scrollTo({ top: 0 })
-					},
-					pageSize: PAGE_SIZE
-				}}
-				dataSource={(this.state.processes || []) as any}
-				renderItem={(vote: ({ id: string, data: ProcessMetadata } | string), idx: number) => (
-					<Skeleton avatar title={false} loading={typeof vote != "object"} active>
-						<List.Item
-							key={idx}
-							actions={hideEditControls ? [] : [
-								<IconText icon={CloseCircleOutlined} text="Mark as ended" onClick={() => this.confirmMarkAsEnded((vote as any).id)} key="mark-as-ended" />,
-							]}
-							extra={<img width={272} alt="Header not found" src={((vote as any).data as ProcessMetadata).details.headerImage} />}
-						>
-							<List.Item.Meta
-								avatar={<Avatar src={this.state.entity.media.avatar} />}
-								title={
-									<Link href={`/processes#/${entityId}/${(vote as any).id}`}>
-										<a>{((vote as any).data as ProcessMetadata).details.title["default"]}</a>
-									</Link>
-								}
-								description={((vote as any).data as ProcessMetadata).details.description["default"]}
-							/>
-						</List.Item>
-					</Skeleton>
-				)}
-			/>
-		</div>
-	}
+            <List
+                itemLayout="vertical"
+                size="large"
+                pagination={{
+                    onChange: page => {
+                        this.setState({ startIndex: (page - 1) * PAGE_SIZE })
+                        window.scrollTo({ top: 0 })
+                    },
+                    pageSize: PAGE_SIZE
+                }}
+                dataSource={(this.state.processes || []) as any}
+                renderItem={(vote: ({ id: string, data: ProcessMetadata } | string), idx: number) => (
+                    <Skeleton avatar title={false} loading={typeof vote != "object"} active>
+                        <List.Item
+                            key={idx}
+                            actions={hideEditControls ? [] : [
+                                <IconText icon={CloseCircleOutlined} text="Mark as ended" onClick={() => this.confirmMarkAsEnded((vote as any).id)} key="mark-as-ended" />,
+                            ]}
+                            extra={<img width={272} alt="Header not found" src={((vote as any).data as ProcessMetadata).details.headerImage} />}
+                        >
+                            <List.Item.Meta
+                                avatar={<Avatar src={this.state.entity.media.avatar} />}
+                                title={
+                                    <Link href={`/processes#/${entityId}/${(vote as any).id}`}>
+                                        <a>{((vote as any).data as ProcessMetadata).details.title["default"]}</a>
+                                    </Link>
+                                }
+                                description={((vote as any).data as ProcessMetadata).details.description["default"]}
+                            />
+                        </List.Item>
+                    </Skeleton>
+                )}
+            />
+        </div>
+    }
 
-	renderNotFound() {
-		return <div className="not-found">
-			<h4>Entity or active processes not found</h4>
-			<p>The entity you are looking for cannot be found</p>
-		</div>
-	}
+    renderNotFound() {
+        return <div className="not-found">
+            <h4>Entity or active processes not found</h4>
+            <p>The entity you are looking for cannot be found</p>
+        </div>
+    }
 
-	renderLoading() {
-		return <div>Loading the votes of the entity...  <Spin indicator={<LoadingOutlined />} /></div>
-	}
+    renderLoading() {
+        return <div>Loading the votes of the entity...  <Spin indicator={<LoadingOutlined />} /></div>
+    }
 
-	render() {
-		return <div id="process-view">
-			{
-				this.state.dataLoading ?
-					<div id="page-body" className="center">
-						{this.renderLoading()}
-					</div>
-					:
-					(this.state.entity && this.state.processes && this.state.processes.length) ?
-						<div id="page-body">
-							{this.renderProcessesList()}
-						</div>
-						: <div id="page-body" className="center">
-							{this.renderNotFound()}
-						</div>
-			}
-		</div >
-	}
+    render() {
+        return <div id="process-view">
+            {
+                this.state.dataLoading ?
+                    <div id="page-body" className="center">
+                        {this.renderLoading()}
+                    </div>
+                    :
+                    (this.state.entity && this.state.processes && this.state.processes.length) ?
+                        <div id="page-body">
+                            {this.renderProcessesList()}
+                        </div>
+                        : <div id="page-body" className="center">
+                            {this.renderNotFound()}
+                        </div>
+            }
+        </div >
+    }
 }
 
 
@@ -246,10 +245,10 @@ class ProcessActiveView extends Component<IAppContext, State> {
 // </MainLayout>
 
 const IconText = ({ icon, text, onClick }: { icon: any, text: string, onClick?: () => void }) => (
-	<span className="icon-text" onClick={() => onClick && onClick()}>
-		{createElement(icon, { style: { marginRight: 8 } })}
-		{text}
-	</span>
+    <span className="icon-text" onClick={() => onClick && onClick()}>
+        {createElement(icon, { style: { marginRight: 8 } })}
+        {text}
+    </span>
 );
 
 export default ProcessActiveViewPage
