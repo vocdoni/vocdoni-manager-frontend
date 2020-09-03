39222e0f4b3ab4eebce46cf6635c051d31494402
Òscar Casajuana <elboletaire@gmail.com>
2020-09-03 17:17:08 +0200

    refs #94 Fix posts/view file indentation

--- pages/posts/view.tsx
+++ pages/posts/view.tsx
@@ -30,153 +30,153 @@ const PostViewPage = props => {
 }
 
 type State = {
-  dataLoading?: boolean,
-  postUpdating?: boolean,
-  entity?: EntityMetadata,
-  entityId?: string,
-  postId?: string,
-  newsFeed?: JsonFeed,
-  newsPost?: JsonFeedPost,
-  bootnodes?: GatewayBootNodes,
-  editorState?: any
+    dataLoading?: boolean,
+    postUpdating?: boolean,
+    entity?: EntityMetadata,
+    entityId?: string,
+    postId?: string,
+    newsFeed?: JsonFeed,
+    newsPost?: JsonFeedPost,
+    bootnodes?: GatewayBootNodes,
+    editorState?: any
 }
 
 // Stateful component
 class PostView extends Component<IAppContext, State> {
-  state: State = {}
+    state: State = {}
 
-  async componentDidMount() {
-      await this.init()
-  }
+    async componentDidMount() {
+        await this.init()
+    }
 
-  async init() {
-      const params = location.hash.substr(2).split("/")
-      if (params.length !== 2) {
-          message.error("The requested data is not valid")
-          Router.replace("/")
-          return
-      }
+    async init() {
+        const params = location.hash.substr(2).split("/")
+        if (params.length !== 2) {
+            message.error("The requested data is not valid")
+            Router.replace("/")
+            return
+        }
 
-      const entityId = params[0]
-      const postId = params[1]
+        const entityId = params[0]
+        const postId = params[1]
 
-      this.setState({ entityId, postId }, () => this.refreshMetadata())
-  }
+        this.setState({ entityId, postId }, () => this.refreshMetadata())
+    }
 
-  shouldComponentUpdate() {
-      const params = location.hash.substr(2).split("/")
-      if (params.length !== 2) {
-          message.error("The requested data is not valid")
-          Router.replace("/")
-          return
-      }
+    shouldComponentUpdate() {
+        const params = location.hash.substr(2).split("/")
+        if (params.length !== 2) {
+            message.error("The requested data is not valid")
+            Router.replace("/")
+            return
+        }
 
-      const entityId = params[0]
-      const postId = params[1]
-      if (entityId !== this.state.entityId
-      || postId !== this.state.postId) {
-          this.init()
-      }
+        const entityId = params[0]
+        const postId = params[1]
+        if (entityId !== this.state.entityId
+        || postId !== this.state.postId) {
+            this.init()
+        }
 
-      return true
-  }
+        return true
+    }
 
-  async refreshMetadata() {
-      try {
-          this.props.setMenuSelected("new-post")
+    async refreshMetadata() {
+        try {
+            this.props.setMenuSelected("new-post")
 
-          const params = location.hash.substr(2).split("/")
-          if (params.length !== 2) {
-              message.error("The requested data is not valid")
-              Router.replace("/")
-              return
-          }
+            const params = location.hash.substr(2).split("/")
+            if (params.length !== 2) {
+                message.error("The requested data is not valid")
+                Router.replace("/")
+                return
+            }
 
-          const entityId = params[0]
-          const postId = params[1]
+            const entityId = params[0]
+            const postId = params[1]
 
-          this.setState({ dataLoading: true, entityId, postId })
+            this.setState({ dataLoading: true, entityId, postId })
 
-          const gateway = await getGatewayClients()
-          const entity = await Entity.getEntityMetadata(entityId, gateway)
-          if (!entity) throw new Error()
+            const gateway = await getGatewayClients()
+            const entity = await Entity.getEntityMetadata(entityId, gateway)
+            if (!entity) throw new Error()
 
-          // TODO: MULTILANGUAGE
-          const newsFeedOrigin = entity.newsFeed.default
-          const payload = await fetchFileString(newsFeedOrigin, gateway)
+            // TODO: MULTILANGUAGE
+            const newsFeedOrigin = entity.newsFeed.default
+            const payload = await fetchFileString(newsFeedOrigin, gateway)
 
-          let newsFeed: JsonFeed
-          try {
-              newsFeed = JSON.parse(payload)
-              checkValidJsonFeed(newsFeed)
-          }
-          catch (err) {
-              message.warn("The News Feed does not seem to have a correct format")
-              console.log(err);
-              throw new Error()
-          }
+            let newsFeed: JsonFeed
+            try {
+                newsFeed = JSON.parse(payload)
+                checkValidJsonFeed(newsFeed)
+            }
+            catch (err) {
+                message.warn("The News Feed does not seem to have a correct format")
+                console.log(err);
+                throw new Error()
+            }
 
-          const newsPost = newsFeed.items.find(item => item.id === postId)
-          if (!newsPost) throw new Error()
+            const newsPost = newsFeed.items.find(item => item.id === postId)
+            if (!newsPost) throw new Error()
 
-          this.setState({ newsPost, newsFeed, entity, entityId, postId, dataLoading: false })
-          this.props.setTitle(entity.name.default)
-          this.props.setEntityId(entityId)
-      }
-      catch (err) {
-          this.setState({ dataLoading: false })
-      // throw err
-      }
-  }
+            this.setState({ newsPost, newsFeed, entity, entityId, postId, dataLoading: false })
+            this.props.setTitle(entity.name.default)
+            this.props.setEntityId(entityId)
+        }
+        catch (err) {
+            this.setState({ dataLoading: false })
+        // throw err
+        }
+    }
 
-  renderPost() {
-      const post = this.state.newsPost
-      return <div className="body-card">
-          <Row justify="space-between">
-              <Col xs={24} sm={15}>
-                  <Divider orientation="left">{post.title}</Divider>
-                  <div className="ant-list-item-meta-description">{post.date_published ? new Date(post.date_published).toDateString() : ""}</div>
-                  <br />
-                  {post.content_text && post.content_text.trim() ? post.content_text : <div dangerouslySetInnerHTML={{ __html: post.content_html }} />}
-              </Col>
-              <Col xs={24} sm={8}>
-                  <div style={{ textAlign: "center" }}>
-                      <img width={272} alt={post.title} src={post.image} />
-                  </div>
-              </Col>
-          </Row>
-      </div>
-  }
+    renderPost() {
+        const post = this.state.newsPost
+        return <div className="body-card">
+            <Row justify="space-between">
+                <Col xs={24} sm={15}>
+                    <Divider orientation="left">{post.title}</Divider>
+                    <div className="ant-list-item-meta-description">{post.date_published ? new Date(post.date_published).toDateString() : ""}</div>
+                    <br />
+                    {post.content_text && post.content_text.trim() ? post.content_text : <div dangerouslySetInnerHTML={{ __html: post.content_html }} />}
+                </Col>
+                <Col xs={24} sm={8}>
+                    <div style={{ textAlign: "center" }}>
+                        <img width={272} alt={post.title} src={post.image} />
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    }
 
-  renderNotFound() {
-      return <div className="not-found">
-          <h4>News post not found</h4>
-          <p>The post you are looking for cannot be found</p>
-      </div>
-  }
+    renderNotFound() {
+        return <div className="not-found">
+            <h4>News post not found</h4>
+            <p>The post you are looking for cannot be found</p>
+        </div>
+    }
 
-  renderLoading() {
-      return <div>Loading the details of the entity...  <Spin indicator={<LoadingOutlined />} /></div>
-  }
+    renderLoading() {
+        return <div>Loading the details of the entity...  <Spin indicator={<LoadingOutlined />} /></div>
+    }
 
-  render() {
-      return <div id="post-view">
-          {
-              this.state.dataLoading ?
-                  <div id="page-body" className="center">
-                      {this.renderLoading()}
-                  </div>
-                  :
-                  (this.state.entity && this.state.newsPost) ?
-                      <div id="page-body">
-                          {this.renderPost()}
-                      </div>
-                      : <div id="page-body" className="center">
-                          {this.renderNotFound()}
-                      </div>
-          }
-      </div >
-  }
+    render() {
+        return <div id="post-view">
+            {
+                this.state.dataLoading ?
+                    <div id="page-body" className="center">
+                        {this.renderLoading()}
+                    </div>
+                    :
+                    (this.state.entity && this.state.newsPost) ?
+                        <div id="page-body">
+                            {this.renderPost()}
+                        </div>
+                        : <div id="page-body" className="center">
+                            {this.renderNotFound()}
+                        </div>
+            }
+        </div >
+    }
 }
 
 export default PostViewPage
