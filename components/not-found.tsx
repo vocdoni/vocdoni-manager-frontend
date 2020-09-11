5cd18c7be44bbf2b45a16d3aabc0488e4c76f7f5
Òscar Casajuana <elboletaire@gmail.com>
2020-09-11 19:02:53 +0200

    refs #83 Styled not found errors following other pages styles

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -1,8 +1,11 @@
 // import Link from "next/link"
 import { main } from "../i18n"
 
-const NotFound = props => <div id="not-found">
-    <p>{main.notFound}</p>
+const NotFound = props => <div id="page-body">
+    <div className="not-found body-card">
+        <h4>Oops, this is an error.</h4>
+        <p>{main.notFound}</p>
+    </div>
 </div>
 
 export default NotFound
