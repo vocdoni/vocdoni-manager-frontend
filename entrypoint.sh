3f49874028036f8732388c4642fe7d3a04c7494c
vdo <vdo@greyfaze.net>
2020-10-15 11:37:36 +0200

    Add CI and logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,13 @@
 npm run export
 
 # Copy generated files to nginx's default home
-cp -vr /app/build/* /var/www/html
+cp -r /app/build/* /var/www/html
+
+# forward request and error logs to docker log collector
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
+
+echo "Starting nginx..."
 
 # Run nginx daemon
-exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
+exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
