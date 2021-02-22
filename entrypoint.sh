ff944dbdb9104286d7258a3d8e1910e72e3c8db0
Òscar Casajuana <elboletaire@underave.net>
2021-02-22 18:07:13 +0100

    Changes related to env vars & deployment (#17)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Re-compile static website to /app/build
-npm run export
-
 # Copy generated files to nginx's default home
-cp -r /app/build/* /var/www/html
+cp -fr /app/build/* /var/www/html
 
 # forward request and error logs to docker log collector
 ln -sf /dev/stdout /var/log/nginx/access.log
